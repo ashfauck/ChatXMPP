7902d77f784870767c2bd07274d9f7b36cef0d3a
Ashfauck <ashfauck@gmail.com>
2021-02-16 10:29:13 +0530

    Initial Commit

---
+++ ChatXMPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatXMPP
+//
+//  Created by ashfauck thaminsali on 16/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
